493cee9168fcd3bf9debdbaf5e5201819a7d6654
Jim Bo <jim@rath.us>
2019-06-06 12:24:13 -0500

    first pass at PN532 implementation

---
+++ PN532.cpp
@@ -0,0 +1,182 @@
+#include <stdexcept>
+
+#include "PN532.h"
+
+const SPISettings pn532_spi_settings(1000000, LSBFIRST, SPI_MODE0);
+
+PN532::PN532(pin_size_t cs_pin, bool debug) : _debug(debug) {
+  _spi = SPI_Device(pn532_spi_settings, cs_pin);
+  // try to talk to the PN532
+  _wakeup();
+  uint32_t version;
+  bool success = get_firmware_version(&version);
+  if (success) { return; }
+  // first time often fails; if so, try again
+  success = get_firmware_version(&version);
+  if (success) { return; }
+  else {
+    throw std::runtime_error(F("Could not wake PN532"));
+    return;
+  }
+}
+
+void PN532::_wakeup(void) {
+  // set the CS pin to wake the PN532, and wait for it to wake
+  _spi.enter();
+  delay(WAKE_DELAY);
+  _spi.exit();
+}
+
+bool PN532::_wait_ready(uint16_t timeout=1000) {
+  uint16_t timer = 0;
+  bool ready = false;
+  uint8_t status;
+  _spi.enter();
+  while (!ready && timer < timeout) {
+    status = _spi.transfer(SPI_STATUSREAD);
+    ready = status & SPI_READY;
+    if (!ready) {
+      delay(SPI_STATUSPOLLINTERVAL);
+      timer += SPI_STATUSPOLLINTERVAL;
+    }
+  }
+  _spi.exit();
+  return ready;
+}
+
+void PN532::_read_data(uint8_t * data, uint8_t count) {
+  _spi.enter();
+  _spi.transfer(SPI_DATAREAD);
+  _spi.transfer(data, count);
+  _spi.exit();
+}
+
+void PN532::_write_data(uint8_t * data, uint8_t count) {
+  _spi.enter();
+  _spi.transfer(SPI_DATAWRITE);
+  _spi.transfer(data, count);
+  _spi.exit();
+}
+
+uint8_t PN532::_read_frame(uint8_t * response, uint8_t * count) {
+  const uint8_t frame_length = count+8;
+  uint8_t i, checksum, frame[frame_length];
+  _read_data(frame, frame_length);
+  if (_debug) {
+    Serial.print(F("Read frame: "));
+    for (i=0; i<frame_length, i++) { Serial.print(frame[i], HEX); }
+  }
+  // Swallow all the 0x00 values that precede 0xFF
+  // NB: sometimes frames are missing the preamble
+  uint8_t offset = 0;
+  while (frame[offset] == 0x00) {
+    offset += 1;
+    if (offset == frame_length) { return ERR_BAD_PREAMBLE; }
+  }
+  if (frame[offset] != 0xFF) { return ERR_BAD_PACKET_START; }
+  offset += 1;
+  if (offset == frame_length) { return ERR_EMPTY_PACKET; }
+  // Validate length
+  uint8_t response_length = frame[offset];
+  if (response_length == 0) { return ERR_EMPTY_RESPONSE; }
+  if (response_length + frame[offset+1] != 0) { return ERR_BAD_LENGTH_CHECKSUM; }
+  offset += 2;
+  // Make sure the claimed response length doesn't overrun the read length (with
+  // two bytes for data checksum and postamble)
+  if (response_length + offset > frame_length - 2) { return ERR_LONG_RESPONSE; }
+  // Validate response data checksum (NB: +1 to include DCS)
+  for (i=offset, checksum=0; i<offset+response_length+1; i++) {
+    checksum += frame[i];
+  }
+  if (checksum != 0) { return ERR_BAD_DATA_CHECKSUM; }
+  // Check first and last bytes: did this frame come from the PN532? is there a postamble?
+  if (frame[offset] != PN532TOHOST) { return ERR_BAD_TFI; }
+  if (frame[offset+response_length+1] != POSTAMBLE) { return ERR_BAD_POSTAMBLE; }
+  // Return frame data
+  if (response_length > count) {
+    // NB: this is necessary because the preamble and part of the start code may be missing
+    return ERR_LONG_RESPONSE;
+  }
+  count = response_length;
+  for (i=0; i<response_length; i++) { response[i] = frame[offset+1+i]; }
+  return SUCCESS;
+}
+
+void PN532::_write_frame(uint8_t * data, uint8_t count) {
+  const uint8_t frame_length = count+8;
+  uint8_t i, checksum, frame[frame_length];
+  frame[0] = PREAMBLE;
+  frame[1] = STARTCODE1;
+  frame[2] = STARTCODE2;
+  frame[3] = count + 1;
+  frame[4] = ~count;
+  frame[5] = HOSTTOPN532;
+  for (i=0, checksum = HOSTTOPN532; i<count; i++) {
+    frame[6+i] = data[i];
+    checksum += data[i];
+  }
+  frame[6+count] = ~checksum + 1;
+  frame[7+count] = POSTAMBLE;
+  if (_debug) {
+    Serial.print(F("Writing frame: "));
+    for (i=0; i<frame_length, i++) { Serial.print(frame[i], HEX); }
+    Serial.println();
+  }
+  _write_data(frame, frame_length);
+}
+
+uint8_t PN532::_call_function(uint8_t command, uint8_t * params, uint8_t params_len, uint8_t * response, uint8_t * response_len, uint16_t timeout) {
+  uint8_t i, status, command_data[1+params_len], ack_p[ACK_LEN], response_data[1+(*response_len)];
+  try {
+    // write the command data to the PN532
+    command_data[0] = command;
+    for (i=0; i<params_len; i++) { command_data[i+1] = params[i]; }
+    _write_frame(command_data, 1+params_len);
+    // wait for ACK
+    if (!_wait_ready(timeout)) {
+      status = ERR_ACK_TIMEOUT;
+      throw std::runtime_error(F("timed out waiting for command ACK"));
+    }
+    _read_data(ack_p, ACK_LEN);
+    status = SUCCESS;
+    for (i=0; i<ACK_LEN; i++) {
+      if (ack_p[i] != ACK_FRAME[i]) { status = ERR_NO_COMMAND_ACK; }
+    }
+    if (status) { throw std::runtime_error(F("no ACK after command")); }
+    // wait for response
+    if (!_wait_ready(timeout)) {
+      status = ERR_RESPONSE_TIMEOUT;
+      throw std::runtime_error(F("timed out waiting for command response"));
+    }
+    *response_len += 1
+    status = _read_frame(response_data, response_len);
+    if (status) { throw std::runtime_error(F("bad command response frame")); }
+    if (response_data[0] != command+1) {
+      status = ERR_WRONG_COMMAND_RESPONSE;
+      throw std::runtime_error(F("wrong command response"));
+    }
+    // return the response
+    *response_len -= 1;
+    for (i=0; i<response_len; i++) { response[i] = response_data[i+1]; }
+    return SUCCESS;
+  } catch( const std::runtime_error& e ) {
+    // something went wrong while trying the command:
+    // * send an ACK to abort (and reset communications)
+    // * return the bad status code
+    for (i=0; i<ACK_LEN; i++) { ack_p[i] = ACK_FRAME[i]; }
+    _write_data(ack_p, ACK_LEN);
+    return status;
+  }
+}
+
+bool PN532::get_firmware_version(uint32_t * version) {
+  uint8_t version_length = 4;
+  uint8_t status = _call_function(COMMAND_GETFIRMWAREVERSION, nullptr, 0, version, &version_length, 500);
+  if (_debug && status) {
+    Serial.print(F("COMMAND_GETFIRMWAREVERSION failed: "));
+    Serial.print(status, HEX);
+    Serial.print(F(" "));
+    Serial.println(*version, HEX);
+  }
+  return (status == SUCCESS && version_length == 4);
+}
--- PN532.h
+++ PN532.h
@@ -4,8 +4,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#include "Pin.h"
-#include "SPI_Interface.h"
+#include <Arduino.h>
+
 #include "SPI_Device.h"
 
 class PN532 {
@@ -14,8 +14,8 @@ public:
 
   // METHODS
 
-  PN532(SPI_Interface spi_i, Pin cs_pin, bool debug=false); // UNIMPLEMENTED: Pin irq_pin
-  uint32_t get_firmware_version(void);
+  PN532(pin_size_t cs_pin, bool debug=false); // UNIMPLEMENTED: pin_size_t irq_pin
+  bool get_firmware_version(uint32_t * version);
   bool SAM_disable(void);
   bool read_passive_target_id(uint8_t * uid, uint8_t * uid_len, uint8_t card_baud_rate=ISO14443A_BAUD, uint16_t timeout=1000);
   // UNIMPLEMENTED: ntag2xx_write_block(block_number, data) and ntag2xx_read_block(block_number)
@@ -26,19 +26,38 @@ private:
 
   bool _debug;
   SPI_Device _spi_d;
-  // UNIMPLEMENTED: Pin _irq_pin;
+  // UNIMPLEMENTED: pin_size_t _irq_pin;
 
   // METHODS
 
-  bool _call_function(uint8_t command, uint8_t * params, uint8_t params_len, uint8_t * response, uint8_t * response_len, uint8_t response_len_limit=0, uint16_t timeout=1000);
   void _wakeup(void);
   bool _wait_ready(uint16_t timeout=1000);
   void _read_data(uint8_t * data, uint8_t count);
   void _write_data(uint8_t * data, uint8_t count);
-  bool _read_frame(uint8_t * response, uint8_t * count);
+  uint8_t _read_frame(uint8_t * response, uint8_t * count);
   void _write_frame(uint8_t * data, uint8_t count);
+  uint8_t _call_function(uint8_t command, uint8_t * params, uint8_t params_len, uint8_t * response, uint8_t * response_len, uint16_t timeout=1000);
 
   // CONSTANTS
+
+  enum ERROR_CODES_E : uint8_t {
+    SUCCESS                       = 0x00,
+    // _read_frame
+    ERR_BAD_PREAMBLE              = 0x01,
+    ERR_BAD_PACKET_START          = 0x02,
+    ERR_EMPTY_PACKET              = 0x03,
+    ERR_EMPTY_RESPONSE            = 0x04,
+    ERR_BAD_LENGTH_CHECKSUM       = 0x05,
+    ERR_LONG_RESPONSE             = 0x06,
+    ERR_BAD_DATA_CHECKSUM         = 0x07,
+    ERR_BAD_TFI                   = 0x08,
+    ERR_BAD_POSTAMBLE             = 0x09,
+    // _call_function
+    ERR_ACK_TIMEOUT               = 0x0A,
+    ERR_NO_COMMAND_ACK            = 0x0B,
+    ERR_RESPONSE_TIMEOUT          = 0x0C,
+    ERR_WRONG_COMMAND_RESPONSE    = 0x0D
+  };
 
   // frame markers
   enum FRAME_MARKERS_E : uint8_t {
@@ -55,16 +74,18 @@ private:
   };
 
   // special frames
+  static const uint8_t ACK_LEN       = 6;
   static const uint8_t ACK_FRAME[]   = { 0x00, 0x00, 0xFF, 0x00, 0xFF, 0x00 };
   static const uint8_t NACK_FRAME[]  = { 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00 };
   static const uint8_t ERROR_FRAME[] = { 0x00, 0x00, 0xFF, 0x01, 0xFF, 0x7F, 0x81, 0x00 };
 
-  // SPI protocol
+  // PN532 over SPI protocol: see section 6.2.5 of manual
   enum SPI_E : uint8_t {
-    SPI_STATREAD                  = 0x02,
+    SPI_STATUSREAD                = 0x02,
     SPI_DATAWRITE                 = 0x01,
     SPI_DATAREAD                  = 0x03,
-    SPI_READY                     = 0x01
+    SPI_READY                     = 0x01,
+    SPI_STATUSPOLLINTERVAL        = 5 // milliseconds
   };
 
   // command set
@@ -103,18 +124,10 @@ private:
     COMMAND_TGSETMETADATA         = 0x94
   };
 
-// FIXME: not necessary because the proper response code is always command instruction +1?
-  // command response codes
-  enum RESPONSES_E : uint8_t {
-    RESPONSE_INDATAEXCHANGE       = 0x41,
-    RESPONSE_INLISTPASSIVETARGET  = 0x4B
-  };
-
   // constants for powering down and waking up
   enum POWERDOWN_E : uint8_t {
     POWERDOWN_WAKEFROM_SPI        = 0x20,
-    // dummy byte to send when waiting out the wakeup delay from exiting power down mode
-    WAKEUP_BYTE                   = 0x55,
+    POWERDOWN_DELAY               = 1, // milliseconds
     WAKE_DELAY                    = 2 // milliseconds (max), or 100µs min
   };
 
